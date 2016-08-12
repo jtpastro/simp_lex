0f614bf913424a1f95b4fb29cd4612c7fa4785e8
Jonata Teixeira Pastro <jtpastro@inf.ufrgs.br>
2016-08-12 13:55:22 -0300

    Simple lexical substitution by frequency

--- simple_substitution.py
+++ simple_substitution.py
@@ -2,15 +2,22 @@
 
 import sys, getopt
 from collections import defaultdict, OrderedDict
-from MFS import MFS
+from most_freq_syn import MFS
 
-
-
-def main(argv):
+def main():
     mfs = MFS("base_tep2.txt", "freq")
-    for i in argv:
-        print(mfs.most_freq_syn(i))
+    line_no, total, subs = 0,0,0
+    for line in sys.stdin:
+        line_no += 1
+        for word in line.split():
+            total += 1
+            syn = mfs.most_freq_syn(word)
+            if syn != word:
+                subs += 1
+                print(line_no, ":", word, "->", syn)
+    print("number of words:", total)
+    print("number of substitutions:", subs, str(subs/total*100)+"%")
 	
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
 
