7c4996e83e19771aa97d45169f5951d0c7ffce0a
Jonata Teixeira Pastro <jtpastro@inf.ufrgs.br>
2016-09-12 17:57:12 -0300

    Example of substitution using most frequent synonym. Using pickle to deserialized FreqTEP for speed.

--- simple_substitution.py
+++ simple_substitution.py
@@ -1,22 +1,28 @@
 #!/usr/bin/env python3
 
 import sys, getopt
-from collections import defaultdict, OrderedDict
-from most_freq_syn import MFS
+import pickle
+from FreqTEP import FreqTEP
 
 def main():
-    mfs = MFS("base_tep2.txt.bz2", "freq.bz2")
-    line_no, total, subs = 0,0,0
-    for line in sys.stdin:
-        line_no += 1
-        for word in line.split():
-            total += 1
-            syn = mfs.most_freq_syn(word)
-            if syn != word:
-                subs += 1
-                print(line_no, ":", word, "->", syn)
-    print("number of words:", total)
-    print("number of substitutions:", subs, str(subs/total*100)+"%")
+    with open('FreqTEP.pickle', 'rb') as f:
+        freqtep = pickle.load(f)
+        line_no, total, subs = 0,0,0
+        for line in sys.stdin:
+            line_no += 1
+            for word in line.split():
+                total += 1
+                best_syn = word
+                freq = 0
+                for synset in freqtep.get_synonyms(word):
+                    for syn in synset:
+                        if synset[syn] > freq:
+                            best_syn = syn
+                if best_syn != word:
+                    subs += 1
+                    print(line_no, ":", word, "->", syn)
+        print("number of words:", total)
+        print("number of substitutions:", subs, str(subs/total*100)+"%")
 	
 if __name__ == "__main__":
     main()
